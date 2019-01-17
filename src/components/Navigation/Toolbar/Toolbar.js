8d554da1ca93e3941ffa87b5a17859283ece0a4e
Antarpreet Singh <antarpreet13@gmail.com>
2019-01-16 23:28:09 -0800

    Committed

---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import classes from './Toolbar.css';
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+
+const toolbar = (props) => (
+    <header className={classes.Toolbar}>
+        <div className={classes.DrawerToggle} onClick={props.open}>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+        <div className={classes.Logo}>
+            <Logo />
+        </div>
+        <nav className={classes.DesktopOnly}>
+            <NavigationItems isAuthenticated={props.isAuth}/>
+        </nav>
+    </header>
+);
+
+export default toolbar;
